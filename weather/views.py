af1fed8adfb54dce2504f54c49fc2bae3b7d89a2
islombeknematov <islombekn159@gmail.com>
2021-11-03 16:03:45 +0500

    country_name

--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,6 @@
 import urllib.request
 
+from django.http import Http404
 from django.shortcuts import render
 
 # Create your views here.
@@ -8,23 +9,26 @@ import json
 
 
 def get_weather(request):
-    if request.method == 'POST':
-        city = request.POST['city']
+    try:
+        if request.method == 'POST':
+            city = request.POST['city']
 
-        source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' +
-                                        city + '&units=metric&appid=742370029c8ae0a47ccf91b8a42156fd').read()
+            source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' +
+                                            city + '&units=metric&appid=742370029c8ae0a47ccf91b8a42156fd').read()
 
-        list_data = json.loads(source)
+            list_data = json.loads(source)
+            # print(list_data)
 
-        data = {
-            "country_code": str(list_data['sys']['country']),
-            "coordinate": str(list_data['coord']['lon']) + ' lon' + ', '
-                          + str(list_data['coord']['lat']) + ' lat',
-            "temperature": str(list_data['main']['temp']) + ' `C',
-        }
+            data = {
+                "country_name": str(list_data['name']),
+                "country_code": str(list_data['sys']['country']),
 
-        print(data)
-    else:
-        data = dict()
+                "coord_lon": str(list_data['coord']['lon']) + ' longitude',
+                "coord_lat": str(list_data['coord']['lat']) + ' latitude',
 
-    return render(request, 'WeatherApp.html', data)
+                "temperature": str(list_data['main']['temp']) + ' `C',
+            }
+            return render(request, 'WeatherApp.html', data)
+
+    except:
+        raise Http404
